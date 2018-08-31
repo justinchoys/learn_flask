0348a8bd8e37e6489b4240e3bd2c73f3b8abf0c0
Justin Cho <jchoys@Justins-MacBook-Pro.local>
2018-08-31 16:58:28 -0400

    Implement configuration variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 #app here is name of Flask object
 app = Flask(__name__)
+app.config.from_object(Config)
 
 #import routes file from app folder
 from app import routes
